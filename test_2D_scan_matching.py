8240bb16898a507d53d43b5f867fcc2d1dbe1c6b
Andrey <39188971+TanatosSpirit@users.noreply.github.com>
2019-02-04 15:48:12 +0300

    Modified parser.py to call and added main

---
+++ test_2D_scan_matching.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  4 15:31:58 2019
+
+@author: lytov
+"""
+
+from third_party.parser import parse
+
+filepath = r'D:\workspace\python\NDT_Understanding\ndt_understanding\dataset\sick_dataset.txt'
+scan = parse(filepath)
--- third_party/parser.py
+++ third_party/parser.py
@@ -19,43 +19,50 @@ def spherical2Euclidean(scan):
         scanEucl.append(p)
     return scanEucl
 
-filepath = r'D:\workspace\python\NDT_Understanding\ndt_understanding\dataset\sick_dataset.txt'  
-
-scanRaw = []
-with open(filepath) as fp:  
-   line = fp.readline()
-   while line:
-       singleScan = [float(x) for x in line.split()]
-       scanRaw.append(singleScan)
+def parse(path):
+    scanRaw = []
+    with open(path) as fp:  
        line = fp.readline()
-       
-scanNp = np.array(scanRaw)
+       while line:
+           singleScan = [float(x) for x in line.split()]
+           scanRaw.append(singleScan)
+           line = fp.readline()
+           
+    scanTemp = []
+    for i in range(len(scanRaw)):
+        scanTemp.append(spherical2Euclidean(scanRaw[i][:]))
+    scanXY = np.array(scanTemp)
+    return scanXY
 
-print(len(scanNp[2000,:]))
+def main(path):
+    scanXY = parse(path)
+    # First set up the figure, the axis, and the plot element we want to animate
+    fig = plt.figure(figsize=(10,10))
+    #plt.gca().set_aspect('equal', adjustable='box')
+    ax = plt.axes(xlim=(-35, 35), ylim=(0, 70))
+    line, = ax.plot([], [],'ro')
+    
+    # initialization function: plot the background of each frame
+    def init():
+        line.set_data([], [])
+        return line,
+    
+    # animation function.  This is called sequentially
+    def animate(i):
+        scanForPlot = scanXY[i,:]
+        x, y = scanForPlot.T
+        line.set_data(x, y)
+        return line,
+    
+    # call the animator.  blit=True means only re-draw the parts that have changed.
+    anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                   frames=len(scanXY[:,0]), interval=20, blit=True)
+    
+    #anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+    
+    plt.show()
 
-# First set up the figure, the axis, and the plot element we want to animate
-fig = plt.figure(figsize=(10,10))
-#plt.gca().set_aspect('equal', adjustable='box')
-ax = plt.axes(xlim=(-35, 35), ylim=(0, 70))
-line, = ax.plot([], [],'ro')
-
-# initialization function: plot the background of each frame
-def init():
-    line.set_data([], [])
-    return line,
-
-# animation function.  This is called sequentially
-def animate(i):
-    scanRange = spherical2Euclidean(scanNp[i,:])
-    scanForPlot = np.array(scanRange)
-    x, y = scanForPlot.T
-    line.set_data(x, y)
-    return line,
-
-# call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=len(scanNp[:,0]), interval=20, blit=True)
-
-anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
-
-plt.show()
+if __name__ == '__main__':
+    filepath = r'D:\workspace\python\NDT_Understanding\ndt_understanding\dataset\sick_dataset.txt'
+    main(filepath)
+    
